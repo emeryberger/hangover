45b1a34946da405b2f2e5337581d4b7a484c043a
emery.berger@gmail.com <emery.berger@gmail.com>
2021-03-09 17:57:35 -0500

    Initial.

---
+++ mixdrink.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <random>
+
+int
+main()
+{
+  // Seed with a real random value, if available
+  std::random_device r;
+  
+  std::default_random_engine e1(r());
+  std::uniform_int_distribution<int> uniform_dist(0, RAND_MAX);
+  int rando = uniform_dist(e1);
+  char * rc = (char *) &rando;
+  for (int i = 0; i < 4; i++) {
+    std::cout << rc[i];
+  }
+  std::uniform_int_distribution<int> action_dist(0, 2);
+  char arr[] = { 'M', 'F', 'R' };
+  for (int i = 0; i < 1000; i++) {
+    std::cout << arr[action_dist(e1)];
+  }
+  return 0;
+}
